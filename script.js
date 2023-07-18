43ae1b18eb472f173ed7da0c3f63cc9986602750
Steven Bunde <stevenbunde2020@gmail.com>
2023-07-17 23:35:41 -0400

    Set prompts for entering character preferences

--- script.js
+++ script.js
@@ -8,11 +8,19 @@ function generatePassword() {
 var password = "";
 
 // Set password length prompts
-var size = prompt("How many characters would you like your password to be?");
-if (size < 8 || size > 128) {
+var characters = prompt("How many characters would you like your password to be?");
+if (characters < 8 || characters > 128) {
   alert("Your password must be between 8 and 128 characters to be valid. Please try again!");
   return;
 }
+
+// Set character selection prompts
+var upperCase = confirm("Click YES to include uppercase characters.");
+var lowerCase = confirm("Click YES to include lowercase characters.");
+var numbers = confirm("Click YES to include numbers.");
+var specials = confirm("Click YES to include special characters.");
+
+
 
 
 // Write password to the #password input
