255a648251d87b128f7ae9aab9f7cf3c9c064ef5
Steven Bunde <stevenbunde2020@gmail.com>
2023-07-17 23:22:58 -0400

    Created comments and alert message for prompt

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Generate password function
+function generatePassword() {
+
+// Defined password variable
+var password = "";
+
+// Set password length prompts
+var size = prompt("How many characters would you like your password to be?");
+if (size < 8 || size > 128) {
+  alert("Your password must be between 8 and 128 characters to be valid. Please try again!");
+  return;
+}
+
 
 // Write password to the #password input
 function writePassword() {
@@ -13,5 +24,7 @@ function writePassword() {
 
 }
 
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
